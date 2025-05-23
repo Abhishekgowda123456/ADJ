ef9bcbdedb6638bee97a895309824c120254a5db
Abhishek gowda GR <152285255+Abhishekgowda123456@users.noreply.github.com>
2025-05-23 19:43:30 +0600

    Update SwapElements.java

--- 1c/SwapElements.java
+++ 1c/SwapElements.java
@@ -1,3 +1,8 @@
+/* Write a Java program that swaps two elements ( first and third elements ) in a linked list 
+( using Collections.swap(l_list, 0, 2))*/
+
+
+
 package JavaCollection;
 
 import java.util.*;
